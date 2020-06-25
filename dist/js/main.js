02953a7678c1f6660da61cbb99cd66b83c30984a
Даня <diman100l@mail.ru>
2020-06-25 20:41:52 +0500

    create project

---
+++ dist/js/main.js
@@ -0,0 +1,51 @@
+$(function () {
+    $('.specialities-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+    })
+
+    $('.ham-menu').on('click', function () {
+        $(this).toggleClass('active')
+        $('.nav__list').toggleClass('active')
+    })
+
+    $('.flowing-scroll').on('click', function(){ 
+        let el = $(this);
+        let dest = el.attr('href'); // получаем направление
+        if(dest !== undefined && dest !== '') { // проверяем существование
+            $('html, body').animate({ 
+                scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+            }, 500 // скорость прокрутки
+            );
+        }
+        return false;
+    });
+
+    
+
+    const $bookUpform = $('#bookUpForm')
+    const $sendMessage = $('#sendMessage')
+    if ($bookUpform.length) {
+        $bookUpform.validate({
+            rules: {
+                firstname: {
+                    required: true
+                }
+            },
+            
+            message: {
+                required: null
+            },
+        })
+    }
+    if ($sendMessage.length) {
+        $sendMessage.validate({
+            rules: {
+                required: true
+            }
+        })
+    }
+});
